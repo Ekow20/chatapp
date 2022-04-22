f7e9f547b5af6365637b1f5c1c76193493ad4e36
Ekow <ekown20@gmail.com>
2022-04-22 16:29:54 +0000

    functional

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  variants: {
-    extend: { backgroundColor: ["dark"], textColor: ["dark"] },
-  },
+
   theme: {
     minWidth: {
       6: "70px",
